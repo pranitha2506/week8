b8650d7036a5656662a8d0ec56012decefd2d1f2
pranitha2506 <padthampranitha2004@gmail.com>
2026-09-21 09:55:26 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form['name']
+
+    year = request.form['year']
+
+    return render_template(
+        'successful.html',
+        name=name,
+        year=year
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
